f613aab2cd6ac4a06034a38e108d70c4a8132d75
Benji <24bbaruch@athenian.org>
2023-05-13 22:07:18 -0700

    Changed font and tweaked UI

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -22,7 +22,7 @@ public class GamePanel extends JPanel {
         timeLimit = 10;
         time = 0;
         try {
-            font = Font.createFont(Font.TRUETYPE_FONT, new File("C:\\Users\\ultra\\IdeaProjects\\APCS Final Proj 2023\\data\\Carnevalee Freakshow.ttf"));
+            font = Font.createFont(Font.TRUETYPE_FONT, new File("data/Hegimeda.otf"));
         } catch (IOException | FontFormatException e) {
             System.out.println(e.toString());
             font = Font.getFont(Font.SERIF);
@@ -84,7 +84,7 @@ public class GamePanel extends JPanel {
         wY = d.height;
         Graphics2D g2 = (Graphics2D) g;
         int pSize = (int)((double) Math.min(wX, wY) * 0.8 / Math.max(6, prompt.length()));
-        int pX = (int)((double) pSize * prompt.length() / -2);
+        int pX = (int)((double) pSize * prompt.length() / -2) + wX/2;
         int pY = pSize * 2;
         int pArc = pSize / 6;
         int segmentsTotal = 150;
@@ -95,9 +95,9 @@ public class GamePanel extends JPanel {
         g2.setFont(font.deriveFont(pSize * 0.8f));
         for (int i = 0; i < prompt.length(); i++) {
             g2.setColor(promptColors[i]);
-            g2.fillRoundRect(pX, pY, pSize, pSize, pArc, pArc);
+            g2.fillRoundRect(pX + (int)(pSize * 1.2 * i), pY, pSize, pSize, pArc, pArc);
             g2.setColor(Color.BLACK);
-            g2.drawString(prompt.substring(i,i+1), pX, pY);
+            g2.drawString(prompt.substring(i,i+1), pX + (int)(pSize * 1.2 * (i + 0.3)), pY + (int)(pSize * 0.7));
         }
         g2.setStroke(new BasicStroke(5F));
         if (segmentsCurrent > segmentsTotal / 2) g2.setColor(new Color(Math.min(255, Math.max(0, 512-(int)(512*segmentsCurrent/segmentsTotal))), 255, 0));
